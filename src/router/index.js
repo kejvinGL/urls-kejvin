3de3a16fcb5789d04858040d36613180eefee519
kejvinGL <kejvinbraka@gmail.com>
2023-08-06 15:59:29 +0200

    fix urls

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,17 @@ import Redirect from '../views/Redirect.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
+    {
+      path: '/',
+      name: 'home',
+      component: SiteView
+    },
     {
       path: '/:code',
       name: "redirect",
       component: Redirect,
     },
-    {
-      path: '/',
-      name: 'home',
-      component: SiteView
-    }
   ]
 })
 
