0aec085f1776e6c1e90b7ee8c70e28d0d309bcfd
kejvinGL <kejvinbraka@gmail.com>
2023-08-06 17:46:54 +0200

    fix urls

---
+++ src/views/SiteView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-urlS-primary">
+    <createUrl />
+    <urlTable />
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import createUrl from "../components/createUrl.vue";
+import urlTable from "../components/urlTable.vue";
+</script>
