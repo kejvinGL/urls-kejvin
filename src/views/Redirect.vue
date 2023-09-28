75872c9514088cf699fc17b039a62ac725b4f8d5
kejvin <kejvinbraka@gmail.com>
2023-09-28 16:57:25 +0200

    changed to new server

--- src/views/Redirect.vue
+++ src/views/Redirect.vue
@@ -1,12 +1,10 @@
-<template>
-  <div></div>
-</template>
+<template></template>
 
 <script>
 export default {
   beforeRouteEnter(to, from, next) {
     const code = to.params.code; // Get the code from the route parameter
-    const externalURL = `https://urls-kejvin-server-f69fa1281575.herokuapp.com/${code}`; // Replace "example.com" with the actual base external URL
+    const externalURL = `https://urls-kejvin-server.onrender.com/${code}`; // Replace "example.com" with the actual base external URL
 
     // Redirect the user to the external URL
     window.location.href = externalURL;
